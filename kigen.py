8c0af231a42141316fcab9bd39c837bb6792d80d
Jeff Ciesielski <jeff.ciesielski@key.me>
2018-08-06 07:29:08 -0400

    Initial commit
    Extraction core is there, so we can grab autogen blocks out of strings
    now.  Next step is to add generation, then the plugin system (for
    loading templates from folders) and a CLI.

---
+++ kigen.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+import collections
+import os
+import pkgutil
+import sys
+
+import jinja2
+
+START_MARKER = 'KIGEN_start'
+STOP_MARKER = 'KIGEN_end'
+
+ModuleCmd = collections.namedtuple('ModuleCmd', 'function args')
+AutogenBlock = collections.namedtuple('AutogenBlock',
+                                      'start end command commentmark')
+
+
+class NestedBlockError(Exception):
+    pass
+
+
+class DanglingBlockEnd(Exception):
+    pass
+
+
+def split_marker(line) -> str:
+    """Returns a tuple containing the comment marker (i.e. // or #) and a
+    line absent the start marker and any preceeding comment indicators
+
+    eg: '# KIGEN_start foo bar:baz' -> ('#', 'foo bar:baz')
+
+    """
+    idx = line.index(START_MARKER)
+    return line[:idx].strip(), line[idx + len(START_MARKER):].strip()
+
+
+def extract_args(raw_args):
+    """Given a list of strings, where each string is in the form
+    "key:value", returns a dictionary mapping keys to values
+
+    """
+    result = collections.OrderedDict()
+
+    for arg in raw_args:
+        k, v = arg.split(':')
+        result[k] = v
+    return result
+
+
+def extract_command(line):
+    # Remove the marker leaving us with just a function and its
+    # (optional) arguments
+    commentmarker, line = split_marker(line)
+
+    # Remove any pesky whitespace
+    line = line.strip()
+
+    function, *raw_args = line.split(' ')
+    args = extract_args(raw_args)
+
+    return commentmarker, ModuleCmd(function, args)
+
+
+def extract_blocks(file_data):
+    result = []
+    in_block = False
+    block_start = 0
+    command = None
+    commentmarker = None
+    for idx, line in enumerate(file_data.splitlines()):
+        if START_MARKER in line:
+            if in_block:
+                raise NestedBlockError(
+                    "Nested blocks are not supported. "
+                    "Detected a new block at line {} "
+                    "but the block startint at {} has not "
+                    "yet been closed"
+                    .format(idx, block_start)
+                )
+            in_block = True
+            block_start = idx
+            commentmarker, command = extract_command(line)
+        if STOP_MARKER in line:
+            if not in_block:
+                raise DanglingBlockEnd(
+                    "The block end at {} has no beginning!"
+                    .format(idx)
+                )
+            in_block = False
+            result.append(AutogenBlock(block_start, idx,
+                                       command, commentmarker))
+    return result
+
+
+def split_file_at_blocks(file_data, blocks):
+    result = []
+    index = 0
+    file_lines = file_data.splitlines()
+    for block in blocks:
+        result.append('\n'.join(file_lines[index:block.start]))
+        index = block.end + 1
+    return result
+
+
+def file_path_to_base(path: str) -> str:
+    """Given any file path (relative or absolute) returns just the
+    filename without an extension
+
+    """
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+def enumerate_modules_in_dir(path: str):
+    """Given the path to a directory, returns a list of strings
+    representing the autogen modules in the folder.
+
+    An autogen module is defined as a pair of files with the same base
+    name, but with the extensions .py and .jinja2
+
+    """
+    assert os.path.isdir(path)
+
+    files = [x for x in os.listdir(path) if os.path.isfile(x)]
+
+    py_files = [file_path_to_base(x)
+                for x in files if x.endswith('.py')]
+    jinja_files = [file_path_to_base(x)
+                   for x in files if x.endswith('.jinja2')]
+
+    return list(set(py_files).intersection(set(jinja_files)))
+
+
+# Adapted from https://stackoverflow.com/questions/1057431
+def load_modules(path):
+    result = {}
+    for importer, package_name, _ in pkgutil.iter_modules([path]):
+        full_package_name = '{}'.format(package_name)
+        if full_package_name not in sys.modules:
+            module = (importer.find_module(package_name)
+                      .load_module(full_package_name))
+            print("Loading module: {}".format(module.__name__))
+            result[module.__name__] = module
+    return result
+
+
+def main(input_file, module_path):
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ test_kigen.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+from unittest.mock import patch
+import unittest
+
+import kigen
+
+_test_block = """This is just a test
+# KIGEN_start foo_func arg1:a arg2:b arg3:c
+# Yeah, just a comment
+# WOO!
+# KIGEN_end
+Some useless stuff
+// C style inline comments work too
+// KIGEN_start bar_func arg1:a arg2:c arg3:e
+int main() {
+  dothebusiness();
+}
+// KIGEN_end
+
+Need to test empty blocks
+## KIGEN_start baz_func
+## KIGEN_end
+"""
+
+_test_block_content = [
+    "This is just a test",
+    "Some useless stuff\n// C style inline comments work too",
+    "\nNeed to test empty blocks"
+]
+
+_test_block2 = """# KIGEN_start foo_func
+# KIGEN_end
+This is just a test
+# KIGEN_start foo_func arg1:a arg2:b arg3:c
+# Yeah, just a comment
+# WOO!
+# KIGEN_end
+Some useless stuff
+// C style inline comments work too
+// KIGEN_start bar_func arg1:a arg2:c arg3:e
+int main() {
+  dothebusiness();
+}
+// KIGEN_end
+
+Need to test empty blocks
+## KIGEN_start baz_func
+## KIGEN_end
+"""
+
+_test_block2_content = [
+    '',
+    "This is just a test",
+    "Some useless stuff\n// C style inline comments work too",
+    "\nNeed to test empty blocks"
+]
+
+_block_positions = (
+    (1, 4),
+    (7, 11),
+    (14, 15)
+)
+
+_block_funcs = (
+    'foo_func',
+    'bar_func',
+    'baz_func'
+)
+
+_block_args = (
+    {'arg1': 'a', 'arg2': 'b', 'arg3': 'c'},
+    {'arg1': 'a', 'arg2': 'c', 'arg3': 'e'},
+    {}
+)
+
+_module_dir_data = {
+    'fake_directory_listing': [
+        'foo.py',
+        'foo.jinja2',
+        'bar.py',
+        'bar.jinja2',
+        'baz.py',
+        'baz.jinja2',
+        'random_junk.exe'
+    ],
+    'expected_modules': [
+        'foo',
+        'bar',
+        'baz'
+    ]
+}
+
+
+class TestKiGen(unittest.TestCase):
+    def test_block_extraction(self):
+        blocks = kigen.extract_blocks(_test_block)
+        assert len(blocks) == len(_block_positions)
+
+        for idx, block in enumerate(blocks):
+            assert block.start == _block_positions[idx][0]
+            assert block.end == _block_positions[idx][1]
+            assert block.command.function == _block_funcs[idx]
+            assert len(block.command.args) == len(_block_args[idx])
+            for arg in block.command.args:
+                assert block.command.args[arg] == _block_args[idx][arg]
+
+    @patch('os.path.isfile', autospec=True)
+    @patch('os.path.isdir', autospec=True)
+    @patch('os.listdir', autospec=True)
+    def test_module_extraction(self, listdir, isdir, isfile):
+        listdir.return_value = _module_dir_data['fake_directory_listing']
+        isdir.return_value = True
+        isfile.return_value = True
+
+        modules = kigen.enumerate_modules_in_dir('/fake/path/to/module')
+        assert sorted(modules) == sorted(_module_dir_data['expected_modules'])
+
+    def test_split_at_blocks_lead_in(self):
+        blocks = kigen.extract_blocks(_test_block)
+        file_chunks = kigen.split_file_at_blocks(_test_block, blocks)
+        assert file_chunks == _test_block_content
+
+    def test_split_at_blocks_no_lead_in(self):
+        blocks = kigen.extract_blocks(_test_block2)
+        file_chunks = kigen.split_file_at_blocks(_test_block2, blocks)
+        assert file_chunks == _test_block2_content
